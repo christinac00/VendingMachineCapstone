f4a2914f26e20c6f660848791e0acf9be1fb4aec
Samuel Muskovitz <samuel.muskovitz@gmail.com>
2022-06-09 09:49:36 -0400

    Edited Test File

--- src/test/java/com/techelevator/fileaccessor/FileAccessorTest.java
+++ src/test/java/com/techelevator/fileaccessor/FileAccessorTest.java
@@ -8,38 +8,40 @@ import java.io.File;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class FileAccessorTest extends TestCase {
 
     @Test
     public void testGetInventory() {
-        List<Item> inventory = FileAccessor.getInventory(new File("vendingmachine.csv"));
+        Map<String, Item> inventory = FileAccessor.getInventory(new File("vendingmachine.csv"));
 
-        List<Item> actualInventory = new ArrayList<>();
-        actualInventory.add(new Chips("Potato Crisps", BigDecimal.valueOf(3.05), "A1"));
-        actualInventory.add(new Chips("Stacker", BigDecimal.valueOf(1.45), "A2"));
-        actualInventory.add(new Chips("Grain Waves", BigDecimal.valueOf(2.75), "A3"));
-        actualInventory.add(new Chips("Cloud Popcorn", BigDecimal.valueOf(3.65), "A4"));
+        Map<String, Item> actualInventory = new TreeMap<>();
+        actualInventory.put("A1", new Chips("Potato Crisps", BigDecimal.valueOf(3.05), "A1"));
+        actualInventory.put("A2", new Chips("Stacker", BigDecimal.valueOf(1.45), "A2"));
+        actualInventory.put("A3", new Chips("Grain Waves", BigDecimal.valueOf(2.75), "A3"));
+        actualInventory.put("A4", new Chips("Cloud Popcorn", BigDecimal.valueOf(3.65), "A4"));
 
-        actualInventory.add(new Candy("MoonPie", BigDecimal.valueOf(1.80), "B1"));
-        actualInventory.add(new Candy("Cowtales", BigDecimal.valueOf(1.50), "B2"));
-        actualInventory.add(new Candy("Wonka Bar", BigDecimal.valueOf(1.50), "B3"));
-        actualInventory.add(new Candy("Crunchie", BigDecimal.valueOf(1.75), "B4"));
+        actualInventory.put("B1", new Candy("MoonPie", BigDecimal.valueOf(1.80), "B1"));
+        actualInventory.put("B2", new Candy("Cowtales", BigDecimal.valueOf(1.50), "B2"));
+        actualInventory.put("B3", new Candy("Wonka Bar", BigDecimal.valueOf(1.50), "B3"));
+        actualInventory.put("B4", new Candy("Crunchie", BigDecimal.valueOf(1.75), "B4"));
 
-        actualInventory.add(new Beverage("Cola", BigDecimal.valueOf(1.25), "C1"));
-        actualInventory.add(new Beverage("Dr. Salt", BigDecimal.valueOf(1.50), "C2"));
-        actualInventory.add(new Beverage("Mountain Melter", BigDecimal.valueOf(1.50), "C3"));
-        actualInventory.add(new Beverage("Heavy", BigDecimal.valueOf(1.50), "C4"));
+        actualInventory.put("C1", new Beverage("Cola", BigDecimal.valueOf(1.25), "C1"));
+        actualInventory.put("C2", new Beverage("Dr. Salt", BigDecimal.valueOf(1.50), "C2"));
+        actualInventory.put("C3", new Beverage("Mountain Melter", BigDecimal.valueOf(1.50), "C3"));
+        actualInventory.put("C4", new Beverage("Heavy", BigDecimal.valueOf(1.50), "C4"));
 
-        actualInventory.add(new Gum("U-Chews", BigDecimal.valueOf(0.85), "D1"));
-        actualInventory.add(new Gum("Little League Chew", BigDecimal.valueOf(0.95), "D2"));
-        actualInventory.add(new Gum("Chiclets", BigDecimal.valueOf(0.75), "D3"));
-        actualInventory.add(new Gum("Triplemint", BigDecimal.valueOf(0.75), "D4"));
+        actualInventory.put("D1", new Gum("U-Chews", BigDecimal.valueOf(0.85), "D1"));
+        actualInventory.put("D2", new Gum("Little League Chew", BigDecimal.valueOf(0.95), "D2"));
+        actualInventory.put("D3", new Gum("Chiclets", BigDecimal.valueOf(0.75), "D3"));
+        actualInventory.put("D4", new Gum("Triplemint", BigDecimal.valueOf(0.75), "D4"));
 
 
         boolean allEqual = true;
-        for(int i = 0; i < inventory.size(); i++){
-            if(!inventory.get(i).equals(actualInventory.get(i))){
+        for(String key : inventory.keySet()){
+            if(!inventory.get(key).equals(actualInventory.get(key))){
                 allEqual = false;
             }
         }
